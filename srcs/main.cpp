3a32e02b47f18afa55fa78ba035f3ad58a052864
Eliza Sikira <elsikira@f2r3s15.clusters.42paris.fr>
2025-12-09 20:39:08 +0100

    starting of hpp architecture

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,7 +2,10 @@
 
 int	main(int argc, char **argv) {
 	if (argc != 3){
-		std::cerr << "Error\n";
+		std::cerr << "Error : ft_irc program needs 2 arguments. Usage : ./ft_irc <port> <password> \n";
 	}
+
+	
+
 	(void)argv;
 }
